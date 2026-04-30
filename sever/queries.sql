977dc9434a5d481efaf62b570c93102d5e135642
s2520445 <158192956+s2520445s@users.noreply.github.com>
2026-04-29 22:55:52 -0700

    Update queries.sql

--- sever/queries.sql
+++ sever/queries.sql
@@ -1,1 +1,10 @@
+-- Count total data
+SELECT COUNT(*) FROM sensor_data_virtual;
 
+-- View sample data
+SELECT * FROM sensor_data_virtual LIMIT 5;
+
+-- Extract moisture (example)
+SELECT payload::json->>'Moisture Meter - moisture'
+FROM sensor_data_virtual
+LIMIT 10;
