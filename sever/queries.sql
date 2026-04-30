3c1f70cbe1ba67438b7aa72e0c297e3c919442bc
s2520445 <158192956+s2520445s@users.noreply.github.com>
2026-04-29 23:03:16 -0700

    Update queries.sql

--- sever/queries.sql
+++ sever/queries.sql
@@ -9,11 +9,14 @@ SELECT payload::json->>'Moisture Meter - moisture'
 FROM sensor_data_virtual
 LIMIT 10;
 
+-- Moisture
 SELECT AVG((payload::json->>'Moisture Meter - moisture')::float) AS avg_moisture
 FROM sensor_data_virtual;
 
+-- water_usage
 SELECT AVG((payload::json->>'water_usage')::float) AS avg_water
 FROM sensor_data_virtual;
 
+-- Electricity usage
 SELECT AVG((payload::json->>'current')::float) AS avg_energy
 FROM sensor_data_virtual;
