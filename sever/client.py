23fa3854bf82a92b85781c824c699e166d0547d0
s2520445 <158192956+s2520445s@users.noreply.github.com>
2026-04-29 22:56:17 -0700

    Update client.py

--- sever/client.py
+++ sever/client.py
@@ -1,1 +1,15 @@
+import socket
 
+HOST = '127.0.0.1'
+PORT = 65432
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+message = "Hello from client"
+client.sendall(message.encode())
+
+data = client.recv(1024)
+print("Received:", data.decode())
+
+client.close()
