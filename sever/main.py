6effb71b52100aab3ae4da98e9fbb70e31aa5b9f
s2520445 <158192956+s2520445s@users.noreply.github.com>
2026-04-29 22:56:08 -0700

    Update main.py

--- sever/main.py
+++ sever/main.py
@@ -1,1 +1,18 @@
+import socket
 
+HOST = '127.0.0.1'
+PORT = 65432
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+print("Server is running...")
+
+while True:
+    conn, addr = server.accept()
+    print(f"Connected by {addr}")
+    data = conn.recv(1024)
+    print("Received:", data.decode())
+    conn.sendall(b"Data received")
+    conn.close()
